ee9a881cfd09eb3e09739d455cc9c696fbb2dcb4
Jonas Myhr Refseth <jonas@oisann.net>
2018-04-26 23:29:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Update Portfolio On Webhook
+Downloads and stores the PORTFOLIO file of the repo it recieves a webhook from.
+
+
+## Environment Variables
+- SECRET *(Same as in the webhook settings on Github)*
+- DB_FOLDER *(Folder to store the PORTFOLIO files)*
+
+## Docker
+
+`docker run --rm -it -e SECRET=somereallyobscuresecretthatnobodywillguess -e DB_FOLDER=/home/portfolio -v ${PWD}:/home/portfolio -v ~/.ssh/:/root/.ssh/ -p 80:80 oisann/update-portfolio-on-webhook:latest`
+
+This container updates its internal database when it recieves a webhook. It actually just downloads the raw PORTFOLIO file in the root of the repo on the master branch, and stores it in the /home/portfolio folder. 
+
+![alt text](https://i.imgur.com/QL1fR7K.png "Example Github webhook settings")
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func serveGithubWebhook(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	logError(err)
+	secret := []byte(os.Getenv("SECRET"))
+	message := []byte(body)
+	hash := hmac.New(sha1.New, secret)
+	hash.Write(message)
+
+	hashResult := hex.EncodeToString(hash.Sum(nil))
+
+	remoteHash := r.Header.Get("X-Hub-Signature")
+
+	if ("sha1=" + hashResult) == remoteHash {
+		fmt.Println("Hashes matched, ran command!")
+	} else {
+		var y map[string]interface{}
+		json.Unmarshal(body, &y)
+		repo := y["repository"].(map[string]interface{})["full_name"]
+		branch := y["repository"].(map[string]interface{})["default_branch"]
+		id := y["repository"].(map[string]interface{})["id"]
+		url := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/PORTFOLIO", repo, branch)
+		file := fmt.Sprintf("%s/%.0f.portfolio", os.Getenv("DB_FOLDER"), id)
+		err := downloadFile(file, url)
+		if err != nil {
+			logError(err)
+		}
+		fmt.Println("Hashes didn't match, doing nothing.")
+	}
+
+	fmt.Fprintf(w, hashResult)
+}
+
+func downloadFile(filepath string, url string) (err error) {
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Repo does not have a PORTFOLIO file in its root folder")
+		out.Close()
+		os.RemoveAll(fmt.Sprintf("%s/%s", os.Getenv("DB_FOLDER"), out.Name()))
+		return nil
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", serveGithubWebhook)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
